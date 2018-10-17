f8421bfbf6977a9fc33202c11d78d31d8b41ac4d
Schofi <33537727+Schofi@users.noreply.github.com>
2018-10-17 14:11:54 +0800

    并查集最优化的。

---
+++ Union Find/UnionFind6.java
@@ -0,0 +1,45 @@
+
+public class UnionFind6 implements UF {
+	private int[] parent;
+	private int[] rank;
+	public UnionFind6(int size) {
+		parent=new int[size];
+		rank=new int[size];
+		for(int i=0;i<size;i++) {
+			parent[i]=i;
+			rank[i]=1;
+		}
+	}
+	@Override
+	public int getSize() {
+		return parent.length;
+	}
+	private int find(int p) {
+		if(p<0||p>=parent.length) {
+			throw new IllegalArgumentException("p is out of bound");
+		}
+		while(p!=parent[p]) {
+			parent[p]=find(parent[p]);
+		}
+		return parent[p];
+	}
+	@Override
+	public boolean isConnected(int p,int q) {
+		return find(p)==find(q);
+	}
+	public void unionElements(int p,int q) {
+		int proot=find(p);
+		int qroot=find(q);
+		if(qroot==proot) {
+			return;
+		}
+		if(rank[qroot]<rank[proot]) {
+			parent[qroot]=proot;
+		}else if(rank[proot]<rank[qroot]) {
+			parent[proot]=qroot;
+		}else {
+			parent[proot]=qroot;
+			rank[qroot]+=1;
+		}
+	}
+}
